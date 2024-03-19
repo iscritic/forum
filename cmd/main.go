540768ddf555136e416678f892f0baa3916e23a2
arthkim <artur@DESKTOP-VMR2FQM>
2024-03-19 13:30:31 +0500

    connecting db

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"forum/internal"
 	"forum/pkg/logger"
 	"log"
@@ -10,13 +11,19 @@ import (
 func main() {
 	lg := logger.NewLogger()
 
+	db, err := sql.Open("sqlite3", "./db/store.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	srv := &http.Server{
 		Addr:    "0.0.0.0:7000",
 		Handler: internal.Routes(lg),
 	}
 
 	lg.InfoLog.Println("Listening serven on http://localhost:7000...")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatalln(err)
 	}
---
+++ sqlite/sqlite.go
@@ -0,0 +1,27 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(path string) (*Storage, error) {
+
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, fmt.Errorf("can't open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("can't connect to database: %w", err)
+	}
+
+	return &Storage{db: db}, nil
+
+}
+
+func (s *Storage) Save(p *Storage.Page)
